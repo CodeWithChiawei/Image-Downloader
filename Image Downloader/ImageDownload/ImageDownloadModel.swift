ad92bdcc1249d700ea9f68488fd914b3bfc5dec1
Hollis Kwan <holliskwan.ios@gmail.com>
2023-01-31 21:07:26 -0800

    Changed print/hello-world to print/bye-world

---
+++ Image Downloader/ImageDownload/ImageDownloadModel.swift
@@ -0,0 +1,12 @@
+//
+//  ImageDownloadModel.swift
+//  Image Downloader
+//
+//  Created by Hollis Kwan on 1/31/23.
+//
+
+import Foundation
+
+func setConstraints() {
+    fatalError("Error setting constraints")
+}
